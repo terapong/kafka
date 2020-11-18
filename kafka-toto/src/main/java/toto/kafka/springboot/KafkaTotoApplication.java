825a81abab460d911cdb314ca2a0bfa33f340bf4
terapong <terapong@toto>
2020-11-18 15:59:28 +0700

    First commit

---
+++ kafka-toto/src/main/java/toto/kafka/springboot/KafkaTotoApplication.java
@@ -0,0 +1,13 @@
+package toto.kafka.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTotoApplication.class, args);
+	}
+
+}
---
+++ kafka-toto/src/test/java/toto/kafka/springboot/KafkaTotoApplicationTests.java
@@ -0,0 +1,13 @@
+package toto.kafka.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
